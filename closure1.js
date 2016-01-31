0bc271689577cc25dc7165c4b145c31b70ce31b5
PUSRISTEK <pusatrisetteknologi@gmail.com>
2016-01-31 21:31:45 +0700

    new sample

---
+++ closure1.js
@@ -0,0 +1,9 @@
+function outerFunction(arg) {
+var variableInOuterFunction = arg;
+function bar() {
+console.log(variableInOuterFunction); // Access a variable from the outer scope
+}
+// Call the local function to demonstrate that it has access to arg
+bar();
+}
+outerFunction('hello closure!');
